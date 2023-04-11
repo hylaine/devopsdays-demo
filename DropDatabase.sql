7ea508f0068b314c0ac9280f6719d49db891d2fc
Chase Cupp <ccupp@hylaine.com>
2023-04-11 16:45:30 -0400

    Adjusting changeset workflow and build process for demo.

--- DropDatabase.sql
+++
@@ -1,9 +0,0 @@
-USE [DevOpsDays]
-GO
-TRUNCATE TABLE dbo.Project
-IF EXISTS (SELECT 1 FROM sys.foreign_keys WHERE NAME = 'Fk_projects_client_id' AND TYPE = 'F')
-	ALTER TABLE dbo.Project DROP CONSTRAINT Fk_projects_client_id;
-TRUNCATE TABLE dbo.Client
-GO
-DROP TABLE dbo.Project
-DROP TABLE dbo.Client
